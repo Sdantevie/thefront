c3e04c809fb7bb402e88883ce2293436930c2fe7
Steven <dstevie9292@gmail.com>
2017-09-16 08:58:59 +0100

    Some Stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import header  from './components/header/Header';
+import Header  from './components/header/Header';
 import home from './components/home/Home';
 import blogLanding from './components/blog/BlogLanding';
+import Footer from './components/footer/Footer';
 import './App.css'
 
 class App extends React.Component {
   render(){
     return(
           <div className="App-Content">
-              <header />
+              <Header />
               <Switch>
                 <Route exact = {true} path="/" component={home} />
                 <Route path="/blog" component={blogLanding} />
               </Switch>
+              <Footer />
           </div>
     );
   }
